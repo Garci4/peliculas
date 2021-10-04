1bea4f9bc8691baa69f8fa88e1be5b2fc3408152
Garci4 <juliangarcia.edi@gmail.com>
2021-10-04 18:29:24 -0300

    :tada: first upload

---
+++ app/Http/Controllers/PeliculaController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelicula;
+use Illuminate\Http\Request;
+
+class PeliculaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $peliculas = Pelicula::latest()->paginate(5);
+
+        return view('peliculas.index', compact('peliculas'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('peliculas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'anio' => 'required',
+            'director' => 'required',
+            'sinopsis' => 'required'
+        ]);
+
+        Pelicula::create($request->all());
+
+        return redirect()->route('peliculas.index')
+            ->with('success', 'Pelicula creada!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pelicula $pelicula)
+    {
+        return view('peliculas.show', compact('pelicula'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelicula $pelicula)
+    {
+        return view('pelicula.edit', compact('pelicula'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pelicula $pelicula)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'anio' => 'required',
+            'director' => 'required',
+            'sinopsis' => 'required'
+        ]);
+
+        $pelicula->update($request->all());
+
+        return redirect()->route('peliculas.index')
+            ->with('success', 'Pelicula modificada!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pelicula $pelicula)
+    {
+        $pelicula->delete();
+
+        return redirect()->route('pelicula.index')
+            ->with('success', 'Pelicula borrada!');
+    }
+}
